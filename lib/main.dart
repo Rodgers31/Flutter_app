9509791bf7be37446b52dd6208542b033efd3e81
roger <otienor986@gmail.com>
2025-11-13 15:36:17 -0600

    Flutter Learn

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer([Color.fromARGB(255, 60, 37, 125), Color.fromARGB(255, 186, 108, 227)]), // Main content
+      ),
+    ),
+  );
+}
+
+
